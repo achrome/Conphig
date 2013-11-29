1b36c26e73f1936c6929dcb663f338c198da68ba
Xtr0 <achromatic.rainbow@gmail.com>
2013-11-30 05:10:18 +0530

    Added XML configurator

--- src/Conphig/Configurators/IniConfigurator.php
+++ src/Conphig/Configurators/IniConfigurator.php
@@ -4,11 +4,15 @@ namespace Conphig\Configurators;
 
 use Conphig\Configuration\Configuration;
 use Conphig\Helpers\ConfiguratorHelper;
+use Conphig\Exceptions\ConfigurationException;
 
 class IniConfigurator extends AbstractConfigurator {
 
 	public function parseConfig() {
 		$this->intermediateConf = parse_ini_file($this->filePath, true);
+		if ($this->intermediateConf === FALSE)
+			throw new ConfigurationException("Could not read the configuration file");
+		
 		$this->configuration = (new ConfiguratorHelper())->createObjFromArray($this->intermediateConf);
 	}
 }
---
+++ src/Conphig/Configurators/XmlConfigurator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Conphig\Configurators;
+
+use Conphig\Exceptions\ConfigurationException;
+use Conphig\Configuration\Configuration;
+
+class XmlConfigurator extends AbstractConfigurator {
+
+	public function parseConfig() {
+		if (!extension_loaded('libxml'))
+			throw new ConfigurationException("libxml is not loaded");
+		
+		$this->intermediateConf = simplexml_load_file($this->filePath);
+		if($this->intermediateConf->getName() !== 'config')
+			throw new ConfigurationException("All configuration must be wrapped inside <config></config>");
+		
+		$this->configuration = $this->createConfigFromXmlElements($this->intermediateConf);
+	}
+
+	protected function createConfigFromXmlElements($node) {
+		$configuration = new Configuration();
+		foreach ($node->children() as $childNode)
+			$configuration->{$childNode->getName()} = ($childNode->count() > 0) ? $this->createConfigFromXmlElements($childNode) : (string) $childNode;
+		return $configuration;
+	}
+}
