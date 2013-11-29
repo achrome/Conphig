8ac504356bfac9f6fe70ff2ef8c4bd16db3400d2
Xtr0 <achromatic.rainbow@gmail.com>
2013-11-30 05:10:51 +0530

    Refactored

---
+++ src/Conphig/Configurators/JsonConfigurator.php
@@ -0,0 +1,9 @@
+<?php
+namespace Conphig\Configurators;
+
+class JsonConfigurator extends AbstractConfigurator {
+	
+	public function parseConfig() {
+		
+	}
+}
---
+++ src/Conphig/Helpers/ConfiguratorHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Conphig\Helpers;
+
+use \InvalidArgumentException;
+use \ReflectionClass;
+use \Exception;
+
+class ConfiguratorHelper {
+
+	public function createObjFromArray($inArray, $objType = 'Conphig\\Configuration\\Configuration', $objArgs = []) {
+		if (!is_array($inArray))
+			throw new InvalidArgumentException("Expected an array, got a " . gettype($inArray));
+		
+		$reflector = new ReflectionClass($objType);
+		$outObj = method_exists($objType, '__construct') ? $reflector->newInstance($objArgs) : $reflector->newInstanceWithoutConstructor();
+		
+		if (!method_exists($outObj, '__set') || !method_exists($outObj, '__get'))
+			throw new Exception("$objType does not implement methods __get and __set");
+		
+		foreach ($inArray as $key => $value)
+			$outObj->$key = is_array($value) ? $this->createObjFromArray($value, $objType, $objArgs) : $value;
+		
+		return $outObj;
+	}
+}
