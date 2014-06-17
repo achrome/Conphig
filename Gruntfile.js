1a5b0d04e64df146592a56492ec01868dcd9aec5
Ashwin Mukhija <achromatic.rainbow@gmail.com>
2014-06-17 18:18:33 +0530

    Fixing travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,26 +9,16 @@
 
 module.exports = function(grunt) {
   grunt.initConfig({
-    phpcs: {
-      application: {
-        dir: 'src'
-      },
-      options: {
-        standard: 'PSR1',
-        report: 'summary'
-      }
-    },
     phpunit: {
       classes: {
         dir: 'tests'
       }
     }
   });
-
-  grunt.loadNpmTasks('grunt-phpcs');
+  
   grunt.loadNpmTasks('grunt-phpunit');
   
   grunt.registerTask('default', function() {
-    grunt.task.run('phpcs', 'phpunit');
+    grunt.task.run('phpunit');
   });
 }
